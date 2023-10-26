55a0b395c45e44d61b9fd61aee2b2138e2b8d754
Justin Clark <jclarksummit@gmail.com>
2023-10-26 14:16:42 -0400

    Add support for maximized LI posts

--- source/js/timezone-fixers/linkedin.js
+++ source/js/timezone-fixers/linkedin.js
@@ -14,15 +14,31 @@ const fixer = new Fixer('LinkedIn', [
   {
     // Example: https://www.linkedin.com/feed/update/urn:li:activity:7123193054789820416/?origin=SHARED_BY_YOUR_PAGES
     name: 'Maximized post -> hover over header',
-    selector: 'div[class*="update-components-text-view break-words"]',
+    selector: 'div[class*="update-components-text-view break-words"] > span:first-child',
     attachTo: node => node,
-    timestamp: node => {
-      return "none";
-    },
-    url: node => {
-      return "none";
-    },
+    timestamp: _ => extractDateFromPostId(getLIPostId()),
+    url: _ => window.location.href
   },
 ])
 
 fixer.start();
+
+function getLIPostId(url) {
+  const liUrl = url || window.location.href;
+  const regex = /([0-9]{19})/;
+  const postId = regex.exec(liUrl)[0];
+  return postId;
+}
+
+function extractTimestampFromId(postId) {
+  // BigInt needed as we need to treat postId as 64 bit decimal. This reduces browser support.
+  const asBinary = BigInt(postId).toString(2);
+  const first41Chars = asBinary.slice(0, 41);
+  const timestamp = parseInt(first41Chars, 2);
+  return new Date(timestamp);
+}
+
+function extractDateFromPostId(postId) {
+  return extractTimestampFromId(postId);
+}
+
